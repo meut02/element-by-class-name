2feb22a637e94b8840db23d0b3b9252906a4f879
ut02 <utsavsimplecool@gmail.com>
2023-07-06 08:49:47 +0530

    queryselector

--- class.js
+++ class.js
@@ -1,11 +1,28 @@
 
-var li=document.getElementsByTagName('li')
-console.log(li)
-li[1].textContent='hello2'
-li[1].style.fontWeight='bold'
-li[1].style.backgroundColor='yellow'
+var header=document.querySelector('#main-header')
+header.style.borderBottom='solid 4px #ccc'
+var input=document.querySelector('input')
+input.value='hello world'
 
-for(var i=0;i<li.length;i++)
+var submit=document.querySelector('input[type="submit"]')
+submit.value='SEND'
+var item=document.querySelector('.list-group-item')
+item.style.color='red'
+
+
+var lastitem=document.querySelector('.list-group-item:last-child')
+lastitem.style.color='blue'
+
+
+var seconditem=document.querySelector('.list-group-item:nth-child(2)')
+seconditem.style.color='cyan'
+
+
+
+var titles=document.querySelectorAll('.title')
+titles[0].textContent='Hello'
+var odd=document.querySelectorAll('li:nth-child(odd)')
+for(var i=0;i<odd.length;i++)
 {
- li[i].style.backgroundColor='grey'
+    odd[i].style.backgroundColor="green"
 }
